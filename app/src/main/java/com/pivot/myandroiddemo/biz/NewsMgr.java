dcf5a4b70de595a235b27649988a2c99fb8344c2
fjm19960930 <2985474861@qq.com>
2021-08-16 17:06:26 +0800

    [FIX] 新闻页数据获取不到问题处理

--- app/src/main/java/com/pivot/myandroiddemo/biz/NewsMgr.java
+++ app/src/main/java/com/pivot/myandroiddemo/biz/NewsMgr.java
@@ -12,14 +12,14 @@ import okhttp3.Response;
  */
 
 public class NewsMgr {
-    private static final String NEWS_URL = "http://v.juhe.cn/toutiao/index?type=%s&key=eb835aee3b2c44168b6aabbc549b9eba";
+    private static final String NEWS_URL = "https://v.juhe.cn/toutiao/index?type=%s&key=eb835aee3b2c44168b6aabbc549b9eba";
 
     /**
      * 请求新闻数据
      */
     public static void requestNewsInfo(String newsType, OnGetNewsFinishListener listener) {
-        String NewsUrl = String.format(NEWS_URL, newsType);
-        ZHttp.get(NewsUrl, new ZResponse<NewsReply>(NewsReply.class) {
+        String newsUrl = String.format(NEWS_URL, newsType);
+        ZHttp.get(newsUrl, new ZResponse<NewsReply>(NewsReply.class) {
             @Override
             public void onSuccess(Response response, NewsReply resObj) {
                 if (listener != null) {
